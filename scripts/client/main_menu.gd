b856d33f9cf2d0b7e018789fcc23d8146f20d5d9
Javier Sanz <javiersanzmoline@gmail.com>
2026-05-18 20:15:35 +0200

    Add credits scene

--- scripts/client/main_menu.gd
+++ scripts/client/main_menu.gd
@@ -10,6 +10,8 @@ extends Control
 @onready var settings_menu: SettingsMenu = $SettingsMenu
 @onready var how_to_play_texture_button: TextureButton = $HowToPlay_TextureButton
 @onready var how_to_play_menu: Control = $HowToPlayMenu
+@onready var credits_texture_button: TextureButton = $Credits_TextureButton
+@onready var credits_menu: Control = $CreditsMenu
 @onready var avatar_control: ActionsPanelAvatar = $UserInfo_HBoxContainer/Avatar_Control
 
 
@@ -27,6 +29,8 @@ func _ready() -> void:
 	settings_menu.hide()
 	how_to_play_texture_button.pressed.connect(func(): how_to_play_menu.show())
 	how_to_play_menu.hide()
+	credits_texture_button.pressed.connect(func(): credits_menu.show())
+	credits_menu.hide()
 	var random_character = Enums.Character.values().pick_random()
 	avatar_control.set_character(random_character if random_character != 0 else Enums.Character.TIGER)
 
